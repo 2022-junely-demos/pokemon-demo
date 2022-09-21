a6d9dd40b99b634e4082577761e422b47126882c
Julie Nisbet <jmnisbet@gmail.com>
2022-09-21 11:53:23 -0700

    add dropdown for type

--- src/hooks/usePokemon.js
+++ src/hooks/usePokemon.js
@@ -1,25 +1,42 @@
 import { useState, useEffect } from 'react';
-import { fetchPokemon } from '../services/pokemon';
+import { fetchPokemon, fetchTypes } from '../services/pokemon';
 
 export function usePokemon() {
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedType, setSelectedType] = useState('bug');
+  const [types, setTypes] = useState([]);
 
   useEffect(() => {
     const loadData = async () => {
+      console.log('running useEffect', Math.random());
       // load the data from the API
       try {
-        const data = await fetchPokemon();
+        const data = await fetchPokemon(selectedType);
         // set the data in state
         setPokemon(data);
         // set loading to false to stop the loader
         setLoading(false);
       } catch (e) {
+        // eslint-disable-next-line no-console
         console.error(e);
       }
     };
     loadData();
+  }, [selectedType]);
+
+  useEffect(() => {
+    const loadTypes = async () => {
+      try {
+        const data = await fetchTypes();
+        setTypes(data.map((type) => type.type));
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.error(e);
+      }
+    };
+    loadTypes();
   }, []);
 
-  return { loading, pokemon };
+  return { loading, pokemon, types, setSelectedType };
 }
